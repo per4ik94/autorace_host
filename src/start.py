d6aabe902e9ce827adb2f8253255a9949849e5af
per4ik <sergej.cerkasin@gmx.de>
2021-10-17 13:47:37 +0200

    Launch-Datei für die misssions angelegt

--- src/start.py
+++ src/start.py
@@ -1,18 +1,29 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import roslib
 import rospy
+import os
 from std_msgs.msg import UInt8
+
+pub_decided_mode = rospy.Publisher('/core/decided_mode', UInt8)
 
 
 def controller(msg):
     pass
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", msg.data)
+    print(msg.data)
+    # os.system('rostopic pub -1 /core/decided_mode std_msgs/UInt8 "data: 2"')
+    pub_decided_mode.publish(msg.data)
+
+
+def test(msg):
+    pass
+    print(msg.data)
 
 
 def subscriber():
     rospy.init_node('start', anonymous=True)
-
-    rospy.Subscriber('/detect/traffic_light', UInt8, controller)
+    # pub_decided_mode.publish("data: 3")
+    rospy.Subscriber('/detect/traffic_light', UInt8, controller, queue_size=1)
+    rospy.Subscriber('/core/decided_mode', UInt8, test, queue_size=1)
     rospy.spin()
 
 
